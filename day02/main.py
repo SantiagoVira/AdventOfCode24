baa92742fced625b80d74c653d5503abf282d5a0
SantiagoVira <santiagovira06@gmail.com>
2024-12-02 01:11:02 -0500

    "optimizing" day 2

--- day02/main.py
+++ day02/main.py
@@ -4,12 +4,19 @@ with open("./input.txt") as f:
 count = 0
 
 
-def is_safe(nums):
-    mult = 1 if nums[1] > nums[0] else -1
+def is_safe(nums, skip=-1):
+    if skip < 2:
+        mult = 1 if nums[2] > nums[not skip] else -1
+    else:
+        mult = 1 if nums[1] > nums[0] else -1
+
     for i in range(1, len(nums)):
-        diff = mult * (nums[i] - nums[i-1])
-        if not (1 <= diff <= 3):
-            return False
+        if i != skip and (skip != 0 or i != 1):
+            prev = nums[i-2] if i == skip + 1 else nums[i-1]
+            diff = mult * (nums[i] - prev)
+
+            if not (1 <= diff <= 3):
+                return False
 
     return True
 
@@ -21,9 +28,7 @@ for line in data:
         count += 1
     else:
         for i in range(len(nums)):
-            new_nums = nums.copy()
-            new_nums.pop(i)
-            if is_safe(new_nums):
+            if is_safe(nums, skip=i):
                 count += 1
                 break
 
