dbbe899afbb0e8c0681c27a632a47adc58a0ab6c
SantiagoVira <santiagovira06@gmail.com>
2024-12-02 00:49:23 -0500

    day 2

--- day02/main.py
+++ day02/main.py
@@ -1,2 +1,31 @@
 with open("./input.txt") as f:
     data = f.readlines()
+
+count = 0
+
+
+def is_safe(nums):
+    mult = 1 if nums[1] > nums[0] else -1
+    for i in range(1, len(nums)):
+        diff = mult * (nums[i] - nums[i-1])
+        if not (1 <= diff <= 3):
+            return False
+
+    return True
+
+
+for line in data:
+    nums = list(map(int, line.split(" ")))
+
+    if is_safe(nums):
+        count += 1
+    else:
+        for i in range(len(nums)):
+            new_nums = nums.copy()
+            new_nums.pop(i)
+            if is_safe(new_nums):
+                count += 1
+                break
+
+
+print(count)
