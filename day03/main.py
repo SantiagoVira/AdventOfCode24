d2a3496e735253ba922df168397374a997ff2ed2
SantiagoVira <santiagovira06@gmail.com>
2024-12-03 00:10:32 -0500

    day 3

--- day03/main.py
+++ day03/main.py
@@ -1,2 +1,21 @@
+import re
+
 with open("./input.txt") as f:
-    data = f.readlines()
+    data = f.read()
+
+pattern = "(mul\(\d+,\d+\))|(do\(\))|(don't\(\))"
+matches = re.findall(pattern, data)
+
+total = 0
+do = True
+
+for m in matches:
+    if m[1]:
+        do = True
+    elif m[2]:
+        do = False
+    elif do:
+        num1, num2 = m[0][4:-1].split(",")
+        total += int(num1) * int(num2)
+
+print(total)
