6418f859cb6365660e33a751485b0dba8607b560
SantiagoVira <santiagovira06@gmail.com>
2024-12-05 00:40:13 -0500

    day 5

--- day03/main.py
+++ day03/main.py
@@ -3,19 +3,19 @@ import re
 with open("./input.txt") as f:
     data = f.read()
 
-pattern = "(mul\(\d+,\d+\))|(do\(\))|(don't\(\))"
+pattern = "mul\(\d+,\d+\)|do\(\)|don't\(\)"
 matches = re.findall(pattern, data)
 
 total = 0
 do = True
 
 for m in matches:
-    if m[1]:
+    if m == "do()":
         do = True
-    elif m[2]:
+    elif m == "don't()":
         do = False
     elif do:
-        num1, num2 = m[0][4:-1].split(",")
+        num1, num2 = m[4:-1].split(",")
         total += int(num1) * int(num2)
 
 print(total)
--- day05/main.py
+++ day05/main.py
@@ -1,2 +1,51 @@
 with open("./input.txt") as f:
-    data = f.readlines()
+    rules, updates = f.read().split("\n\n")
+
+# Each key must occur before every number in the value array
+befores = {}
+
+for r in rules.split():
+    b, a = map(lambda x: int(x.strip()), r.split("|"))
+    if b not in befores:
+        befores[b] = set()
+
+    befores[b].add(a)
+
+total1, total2 = 0, 0
+
+
+def check_valid(nums):
+    is_valid = True
+    already = {}
+    problem = (-1, -1)
+
+    for i in range(len(nums)):
+        n = nums[i]
+        already[n] = i
+        if n in befores:
+            for a in already.keys():
+                if a in befores[n]:
+                    is_valid = False
+                    problem = (already[a], i)
+                    break
+            if not is_valid:
+                break
+
+    return is_valid, problem
+
+
+for u in updates.split():
+    nums = list(map(lambda x: int(x.strip()), [
+                n for n in u.split(",") if n.isnumeric()]))
+
+    is_valid, problem = check_valid(nums)
+    if is_valid:
+        total1 += nums[len(nums)//2]
+    else:
+        while not is_valid:
+            nums[problem[0]], nums[problem[1]
+                                   ] = nums[problem[1]], nums[problem[0]]
+            is_valid, problem = check_valid(nums)
+        total2 += nums[len(nums)//2]
+
+print(total1, total2)
