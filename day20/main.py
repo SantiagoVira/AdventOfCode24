3ba55413e7d8356dabbc4345a820b4f95810397d
SantiagoVira <santiagovira06@gmail.com>
2024-12-20 14:53:50 -0500

    cleaned up

--- day20/main.py
+++ day20/main.py
@@ -8,83 +8,32 @@ y0 = [i for i in range(len(data)) if "S" in data[i]][0]
 x0 = data[y0].index("S")
 
 
-def get_min_path(data, x0, y0):
-    positions = deque([(x0, y0, 0)])
-    min_path = -1
-    seen = set()
-
-    while positions:
-        x, y, num_moves = positions.popleft()
-
-        if (x, y) in seen:
-            continue
-
-        if data[y][x] == "E":
-            min_path = num_moves
-            break
-
-        seen.add((x, y))
-
-        for (nx, ny) in cardinal_moves_indicies(len(data[0]), len(data), x, y):
-            if data[ny][nx] != "#":
-                positions.append((nx, ny, num_moves + 1))
-
-    return min_path
-
-
-moves = []
 positions = deque([(x0, y0)])
 move_indices = {}
+differences = defaultdict(int)
 num_moves = 0
+MAX_CHEAT_LENGTH = 2  # 20
 
 while positions:
     x, y = positions.popleft()
-
-    if (x, y) in move_indices:
-        continue
-
-    moves.append((x, y))
     move_indices[(x, y)] = num_moves
+    num_moves += 1
 
     if data[y][x] == "E":
         break
 
-    num_moves += 1
-
     for (nx, ny) in cardinal_moves_indicies(len(data[0]), len(data), x, y):
-        if data[ny][nx] != "#":
+        if data[ny][nx] != "#" and (nx, ny) not in move_indices:
             positions.append((nx, ny))
 
-
-# path_length = sum(line.count(".") for line in data) + 1
-differences = defaultdict(int)
-
-# for (x, y) in moves:
-#     two_away = cardinal_moves_indicies(
-#         len(data[0]), len(data), x, y, include_all=True, factor=2)
-#     one_away = cardinal_moves_indicies(
-#         len(data[0]), len(data), x, y, include_all=True)
-
-#     for (x1, y1), (x2, y2) in zip(one_away, two_away):
-#         if not (x2 in range(len(data[0])) and y2 in range(len(data))):
-#             continue
-#         if data[y1][x1] != "#" or data[y2][x2] == "#" or ((x1, y1), (x2, y2)) in cheats_seen:
-#             continue
-
-#         cheats_seen.add(((x1, y1), (x2, y2)))
-
-#         i, i2 = move_indices[(x, y)], move_indices[(x2, y2)]
-
-#         if i < i2:
-#             differences[i2 - i - 2] += 1
-
-for (x1, y1) in moves:
+for (x1, y1) in move_indices.keys():
     cheats_seen = set()
     cheat_positions = deque([(x1, y1, 0)])
+
     while cheat_positions:
         x, y, num_moves = cheat_positions.popleft()
 
-        if num_moves > 20 or (x, y) in cheats_seen:
+        if num_moves > MAX_CHEAT_LENGTH or (x, y) in cheats_seen:
             continue
         cheats_seen.add((x, y))
         if data[y][x] != "#":
@@ -97,13 +46,5 @@ for (x1, y1) in moves:
             cheat_positions.append((nx, ny, num_moves + 1))
 
 
-count = 0
-
-for d, c in differences.items():
-    count += c
-
-# for d in sorted(differences.keys()):
-#     print(f"There are {differences[d]} cheats that save {d} picoseconds")
-
-# print(differences)
+count = sum(differences.values())
 print(count)
